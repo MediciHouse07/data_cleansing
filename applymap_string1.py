32d9c02b73f23a629b20a73a87a163d2f7a06d57
Medici House <96524616+MediciHouse07@users.noreply.github.com>
2022-10-19 09:32:36 +0800

    Add files via upload

---
+++ applymap_string1.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 18 20:39:11 2022
+
+@author: medici
+"""
+
+import re
+import os
+import pandas as pd
+from collections import Counter
+
+os.chdir("D:\\DDog\\analysis\\data_cleansing")
+
+path_data = "data\\"
+#import pandas as pd
+#%%
+
+table = pd.read_csv(path_data+"data.csv")
+
+#%%
+
+f = lambda x: x.split('/')[-1]
+table_sub = table[['Path']].applymap(f)
+table_sub.to_csv(path_data+"data_reshaped.csv")
+
+#%%
+f = lambda x: x.split('/')[:-1]
+table_sub = table[['Path']].applymap(f)
+table_sub.to_csv(path_data+"data_reshaped.csv")
+
+#%%
+
+df = pd.DataFrame({'col1':['A','B','C'],'col2':['black berry','green apple','red wine'],'col3':['black','green','red']})
+
+#%%
+f = lambda x: x.strip()
+table['clean'] = table[['Table']].applymap(f)
+
+#%%
+df['col2'] = [a.replace(b, '').strip() for a, b in zip(df['col2'], df['col3'])]
+
+table['Path'] = [a.replace(b, '').strip() for a, b in zip(table['Path'],table['Clean'])]
+
+#[a.replace(b, '').strip() for a, b in zip(table['Path'],table['Clean'])]
+
+#%%
+
+# df['col1'] = df['col1'].replace(0,df['col2'])
+table.Path.replace(0,table.Table,inplace=True)
+
+#table['sub_path'] = table['Path'].replace(0,table['Table'])
+#%%
+# str.isdigit
+path = "pong/Pong.hack" # storage
+# if @ ( is coupled for a symbol, the it is a symbol, give a value for it based on (
+# if it is not coupled, give value from 0
+
+# Symbol dictionary
+array_symbol = []
+array_symbolJmp = []
+array_symbolJmpOri = []
